2764ec8d7381e2500b8192e26802b112d631965c
k-key-k <vaizat01@gmail.com>
2024-12-03 00:39:54 +0300

    commit num generator fix

--- num_generator_with_randint/num_generator_getrandbits.py
+++ num_generator_with_randint/num_generator_getrandbits.py
@@ -4,16 +4,26 @@ import random
 def num_generator_getrandbits(k):
     num = random.getrandbits(k)
 
-    if num < (1 << (k - 1)):
-        num += (1 << (k - 1))
+    if k <= 0:
+        return EOFError("bit most be > 0")
+    elif k <= 1:
+        return random.getrandbits(k)
+    else:
+        if num < (1 << (k - 1)):
+            num += (1 << (k - 1))
 
-    return num
+        return num
 
 
 def odd_num_generator_getrandbits(k):
     num = random.getrandbits(k) | 1
 
-    if num < (1 << (k - 1)):
-        num += (1 << (k - 1))
+    if k <= 0:
+        return EOFError("bit most be > 0")
+    elif k <= 1:
+        return random.getrandbits(k)
+    else:
+        if num < (1 << (k - 1)):
+            num += (1 << (k - 1))
 
-    return num
+        return num
--- num_generator_with_randint/num_generator_k_bit.py
+++ num_generator_with_randint/num_generator_k_bit.py
@@ -2,8 +2,18 @@ import random
 
 
 def num_generator_k_bit(bit):
-    return random.randint(2 << (bit - 2), (2 << bit - 1) - 1)
+    if bit <= 0:
+        return EOFError("bit most be > 0")
+    elif bit <= 1:
+        return random.randint(0, 1)
+    else:
+        return random.randint(2 << (bit - 2), (2 << bit - 1) - 1)
 
 
 def odd_num_generator_k_bit(bit):
-    return (random.randint(2 << (bit - 2), (2 << bit - 1) - 1)) | 1
+    if bit <= 0:
+        return EOFError("bit most be > 0")
+    elif bit <= 1:
+        return random.randint(0, 1)
+    else:
+        return (random.randint(2 << (bit - 2), (2 << bit - 1) - 1)) | 1
